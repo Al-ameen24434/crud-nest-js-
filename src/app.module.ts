a8104834ec16277c6ba6b3715ff71d3597cb559f
al-ameen24434 <alameenabdulrahmon@gmail.com>
2025-11-19 15:41:26 +0100

    auto generate users

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [PostsModule],
+  imports: [PostsModule, UsersModule],
 })
 export class AppModule {}
