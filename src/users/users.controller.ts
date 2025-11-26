c32201375a478e3131977ec576ff1f2b21baf626
al-ameen24434 <alameenabdulrahmon@gmail.com>
2025-11-26 08:27:15 +0100

    inplemented validation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Patch,
   Post,
   Query,
+  ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -27,7 +28,7 @@ export class UsersController {
 
   @Post()
   create(
-    @Body()
+    @Body(ValidationPipe)
     createUserDto: CreateUserDto,
   ) {
     return this.usersService.create(createUserDto);
@@ -36,7 +37,7 @@ export class UsersController {
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
-    @Body()
+    @Body(ValidationPipe)
     userUpdatedto: UpdateUserDto,
   ) {
     return this.usersService.update(id, userUpdatedto);
