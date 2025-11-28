b69e44219dd134b9c81aa5c79da0832a8817dfb5
al-ameen24434 <alameenabdulrahmon@gmail.com>
2025-11-28 19:09:48 +0100

    added imports foremploee and database  module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { UsersModule } from './users/users.module';
 import { TweetModule } from './tweet/tweet.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [PostsModule, UsersModule, TweetModule],
+  imports: [PostsModule, UsersModule, TweetModule, DatabaseModule, EmployeesModule],
 })
 export class AppModule {}
